f75249d44d7ef69e7f4923c538731310c3d5021d
Débora Sete Acosta <sete.debora@gmail.com>
2025-03-25 22:52:06 -0300

    catch and handle error when user is not found

--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -4,25 +4,35 @@ import styles from './ReposList.module.css'
 const ReposList = ({ nomeUsuario }) => {
     const [repos, setRepos] = useState([]);
     const [estaCarregando, setEstaCarregando] = useState(true);
+    const [ocorreuErro, setOcorreuErro] = useState(false);
 
     useEffect(() => {
         setEstaCarregando(true);
+        setOcorreuErro(false);
         fetch(`https://api.github.com/users/${nomeUsuario}/repos`)
-        .then(res => res.json())
+        .then(res => {
+            if (!res.ok) {
+                throw new Error('Usuário não encontrado');
+            }
+            return res.json()
+        })
         .then(resJson => {
             setTimeout(() => {
-                setEstaCarregando()
                 setRepos(resJson);
-
+                setEstaCarregando(false)
             }, 3000)
+        })
+        .catch(e => {
+            setOcorreuErro(true);
+            setEstaCarregando(false);
         })
     }, [nomeUsuario]);
 
     return (
         <div className="container">
-        {estaCarregando ? (
-            <h1>Carregando...</h1>
-            ) : (
+            {estaCarregando && <h1>Carregando...</h1>}
+            {ocorreuErro && <h2 style={{ color: 'red' }}>Usuário não encontrado, tente novamente.</h2>}
+            {!estaCarregando && !ocorreuErro && repos.length > 0 && (
                 <ul className={styles.list}>
                     {/* {repos.map(repositorio => ( */}
                     {repos.map(({ id, name, language, html_url }) => (
